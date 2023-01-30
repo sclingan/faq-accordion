37498b91d0176ae383a61b5f65c60dc7cc26e10e
Scott Clingan <sclingan1@gmail.com>
2023-01-30 14:04:44 -0600

    Adding interactive elements

--- src/App.tsx
+++ src/App.tsx
@@ -10,46 +10,46 @@ function App() {
       <div className="div__card">
          <img className="img__logo" src={logo} alt=""></img>
         <h1>FAQ</h1> 
-        <div className="div__card_question">
+        <div className="div__card_question" id='question_1'>
           <p>How many team members can I invite?</p>
        </div>
-       <button className="btn__accordion"><img src={arrow_icon} alt=""></img></button>
+       <button className="btn__accordion"><img src={arrow_icon} alt="" id='icon_1'></img></button>
        <div className="div__card_answer">
          <p> You can invite up to 2 additional users on the Free plan. There is no limit on 
           team members for the Premium plan.
           </p>
       </div>
-      <div className="div__card_question">
+      <div className="div__card_question" id='question_2'>
           <p>What is the maximum file upload size?</p>
       </div>
-      <button className="btn__accordion"><img src={arrow_icon} alt=""></img></button>
+      <button className="btn__accordion"><img src={arrow_icon} alt="" id='icon_2'></img></button>
 
       <div className="div__card_answer">
           <p>No more than 2GB. All files in your account must fit your allotted storage space.
             </p>
       </div>
-      <div className="div__card_question">
+      <div className="div__card_question" id='question_3'>
          <p>How do I reset my password?</p>
       </div>
-      <button className="btn__accordion"><img src={arrow_icon} alt=""></img></button>
+      <button className="btn__accordion"><img src={arrow_icon} alt="" id='icon_3'></img></button>
 
       <div className="div__card_answer">
          <p>Click “Forgot password” from the login page or “Change password” from your profile page.
          A reset link will be emailed to you.
          </p>
       </div>
-      <div className="div__card_question">
+      <div className="div__card_question" id='question_4'>
          <p>Can I cancel my subscription?</p>
       </div>
-      <button className="btn__accordion"><img src={arrow_icon} alt=""></img></button>
+      <button className="btn__accordion"><img src={arrow_icon} alt="" id='icon_4'></img></button>
 
       <div className="div__card_answer">
          <p>Yes! Send us a message and we’ll process your request no questions asked.</p>
       </div>
-      <div className="div__card_question">
+      <div className="div__card_question" id='question_5'>
          <p>Do you provide additional support?</p>
       </div>
-      <button className="btn__accordion"><img src={arrow_icon} alt=""></img></button>
+      <button className="btn__accordion"><img src={arrow_icon} alt="" id='icon_5'></img></button>
 
       <div className="div__card_answer">
         <p> Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
