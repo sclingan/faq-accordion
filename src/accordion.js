7749f9e589fc0095a36da37cbf6620dc852b21dd
Scott Clingan <sclingan1@gmail.com>
2023-02-04 23:21:04 -0600

    Adding Javascript for accordion

--- src/accordion.js
+++ src/accordion.js
@@ -2,3 +2,6 @@
 // let ans = document.getElementById('answer_1');
 
 // ans.style.display = 'inline';
+
+
+// document.getElementById('answer_2').style.position = 'relative';
--- src/card.tsx
+++ src/card.tsx
@@ -18,7 +18,7 @@ function Card() {
                 </li>
                 <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
-                    <div className='div__card_answer'>
+                    <div className='div__card_answer' id="answer_1">
                         <p>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>
                     </div>
                 </li>
@@ -29,7 +29,7 @@ function Card() {
                 </li>
                 <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
-                    <div className='div__card_answer'>
+                    <div className='div__card_answer' id='answer_2'>
                         <p>No more than 2GB. All files in your account must fit your allotted storage space.</p>
                     </div>
                 </li>
@@ -40,7 +40,7 @@ function Card() {
                 </li>
                 <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
-                    <div className='div__card_answer'>
+                    <div className='div__card_answer' id='answer_3'>
                         <p>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</p>
                     </div>
                 </li>
@@ -51,7 +51,7 @@ function Card() {
                 </li>
                 <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
-                    <div className='div__card_answer'>
+                    <div className='div__card_answer' id='answer_4'>
                         <p>Yes! Send us a message and we’ll process your request no questions asked.</p>
                     </div>
                 </li>
@@ -62,7 +62,7 @@ function Card() {
                 </li>
                 <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
-                    <div className='div__card_answer'>
+                    <div className='div__card_answer' id='answer_5'>
                         <p>Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
                     </div>
                 </li>
