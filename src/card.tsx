d21f205ce85830c6cdf0c390fd12c0feba4aa403
Scott Clingan <sclingan1@gmail.com>
2023-02-04 22:38:16 -0600

    Fixed ul accessibility issue

--- src/card.tsx
+++ src/card.tsx
@@ -16,66 +16,57 @@ function Card() {
                         <p>How many team members can I invite?</p>
                     </div>
                 </li>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
                     <div className='div__card_answer'>
                         <p>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>
                     </div>
                 </li>
-
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
-
                 <li>
                     <div className='div__card_question'>
                         <p>What is the maximum file upload size?</p>
                     </div>
                 </li>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
                     <div className='div__card_answer'>
                         <p>No more than 2GB. All files in your account must fit your allotted storage space.</p>
                     </div>
                 </li>
-
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
-
                 <li>
                     <div className='div__card_question'>
                         <p>How do I reset my password?</p>
                     </div>
                 </li>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
                     <div className='div__card_answer'>
                         <p>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</p>
                     </div>
                 </li>
-
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
-
                 <li>
                     <div className='div__card_question'>
                         <p>Can I cancel my subscription?</p>
                     </div>
-
                 </li>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
                     <div className='div__card_answer'>
                         <p>Yes! Send us a message and we’ll process your request no questions asked.</p>
                     </div>
                 </li>
-
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
-
                 <li>
                     <div className='div__card_question'>
                         <p>Do you provide additional support?</p>
                     </div>
                 </li>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
                 <li>
                     <div className='div__card_answer'>
                         <p>Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
                     </div>
                 </li>
 
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
 
             </ul>
         </section>
