732502ce21151655ee57e1573218a21304962e55
Scott Clingan <sclingan1@gmail.com>
2023-02-06 15:11:27 -0600

    Writing utilities for CSS

--- src/accordion.js
+++ src/accordion.js
@@ -1,5 +1,11 @@
 
 
-// let answer_2 = document.getElementById('answer_2');
+let answer_2 = document.getElementById('answer_2');
 // answer_2.classList.remove('div__card_answer');
 // answer_2.classList.add('#show');
+// let question = document.getElementById('question_2');
+// question.style.borderBottom = 'none';
+// answer_2.style.position = 'relative';
+// answer_2.style.top = '0';
+// answer_2.style.borderBottom = '1px solid black';
+// question.style.borderBottom = 'none';
