ef9238587d18dbae763006350685add463b48a50
Scott Clingan <sclingan1@gmail.com>
2023-02-05 23:13:11 -0600

    Adding utility class for hidden list items

--- src/accordion.js
+++ src/accordion.js
@@ -3,5 +3,13 @@
 
 // ans.style.display = 'inline';
 
+export function changeMe() {
+document.getElementById('answer_2').style.position = 'relative';
+document.getElementById('answer_2').style.top = '0';
+document.getElementById('question_2').style.color = 'black';
+document.getElementById('question_2').style.fontWeight = '900';
+document.getElementById('question_2').style.borderBottom = 'none';
+document.getElementById('icon_2').style.borderBottom = 'none';
+};
 
-// document.getElementById('answer_2').style.position = 'relative';
+// changeMe();
--- src/card.tsx
+++ src/card.tsx
@@ -2,7 +2,6 @@ import logo from '../public/illustration-woman-online-mobile.svg';
 import arrow_icon from '../public/icon-arrow-down.svg';
 
 
-
 function Card() {
     return(
         <div>
@@ -16,14 +15,14 @@ function Card() {
                         <p>How many team members can I invite?</p>
                     </div>
                 </li>
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_1'></img></button>
                 <li>
                     <div className='div__card_answer' id="answer_1">
                         <p>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>
                     </div>
                 </li>
                 <li>
-                    <div className='div__card_question'>
+                    <div className='div__card_question' id='question_2'>
                         <p>What is the maximum file upload size?</p>
                     </div>
                 </li>
@@ -38,7 +37,7 @@ function Card() {
                         <p>How do I reset my password?</p>
                     </div>
                 </li>
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_3'></img></button>
                 <li>
                     <div className='div__card_answer' id='answer_3'>
                         <p>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</p>
@@ -49,7 +48,7 @@ function Card() {
                         <p>Can I cancel my subscription?</p>
                     </div>
                 </li>
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_4'></img></button>
                 <li>
                     <div className='div__card_answer' id='answer_4'>
                         <p>Yes! Send us a message and we’ll process your request no questions asked.</p>
@@ -60,7 +59,7 @@ function Card() {
                         <p>Do you provide additional support?</p>
                     </div>
                 </li>
-                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_2'></img></button>
+                <button className="btn__accordion" aria-expanded='false'><img src={arrow_icon} alt="" id='icon_5'></img></button>
                 <li>
                     <div className='div__card_answer' id='answer_5'>
                         <p>Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
