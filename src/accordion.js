d5d072d0310b05b3fceca07ad17adbdd978be9f0
Scott Clingan <sclingan1@gmail.com>
2023-02-05 23:30:35 -0600

    Using javascript to add and remove utility classes

--- src/accordion.js
+++ src/accordion.js
@@ -1,15 +1,5 @@
 
-// let ans = document.getElementById('answer_1');
 
-// ans.style.display = 'inline';
-
-export function changeMe() {
-document.getElementById('answer_2').style.position = 'relative';
-document.getElementById('answer_2').style.top = '0';
-document.getElementById('question_2').style.color = 'black';
-document.getElementById('question_2').style.fontWeight = '900';
-document.getElementById('question_2').style.borderBottom = 'none';
-document.getElementById('icon_2').style.borderBottom = 'none';
-};
-
-// changeMe();
+// let answer_2 = document.getElementById('answer_2');
+// answer_2.classList.remove('div__card_answer');
+// answer_2.classList.add('#show');
