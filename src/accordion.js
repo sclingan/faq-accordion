9cd81b748682a724f30346d136f107812b60fedc
Scott Clingan <sclingan1@gmail.com>
2023-02-12 16:14:50 -0600

    Refactoring card and ul

--- src/accordion.js
+++ src/accordion.js
@@ -4,20 +4,20 @@
 
 
 // take in number as id
-export default function ShowAnswer() {
-    let question = document.getElementById('question_2');
-    let answer = document.getElementById('answer_2');
-    let chevron = document.getElementById('icon_2');
+// export default function ShowAnswer() {
+//     let question = document.getElementById('question_2');
+//     let answer = document.getElementById('answer_2');
+//     let chevron = document.getElementById('icon_2');
 
     // apply changes in question
     // bold type
     // move underline
 
-    question.style.fontWeight = '700';
-    question.style.borderBottom = 'none';
-    question.style.color = 'var(--text-dark-gray)';
-    question.style.paddingBlock = '0.5rem';
-    question.style.paddingTop = '1rem';
+    // question.style.fontWeight = '700';
+    // question.style.borderBottom = 'none';
+    // question.style.color = 'var(--text-dark-gray)';
+    // question.style.paddingBlock = '0.5rem';
+    // question.style.paddingTop = '1rem';
 
 
 
@@ -25,10 +25,10 @@ export default function ShowAnswer() {
     // apply changes in answer
     // add border underline
     // adjust padding 
-    answer.style.position = 'relative';
-    answer.style.top = '0';
-    answer.style.paddingTop = '0';
-    answer.style.borderBottom = '1px solid var(--dividers-gray-blue)';
+    // answer.style.position = 'relative';
+    // answer.style.top = '0';
+    // answer.style.paddingTop = '0';
+    // answer.style.borderBottom = '1px solid var(--dividers-gray-blue)';
 
 
 
@@ -37,6 +37,6 @@ export default function ShowAnswer() {
     // add padding-left/right
     // add border underline
 
-    chevron.style.border = 'none';
-}
+    // chevron.style.border = 'none';
+// }
 
